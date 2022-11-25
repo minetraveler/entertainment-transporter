4db223b6efe4ba07269722b45fb7b6750cb070c0
decoy <matthew@aviner.com>
2022-11-25 18:30:59 -0500

    changed that last datapoint
    idk why i even added it there but its been changed to how many times you would wait if you wanterd to bet

--- GITHUBgames/projects/casino/code/roll.js
+++ GITHUBgames/projects/casino/code/roll.js
@@ -5,7 +5,6 @@ function roll(){
          //get amount of times for the color to be the same
         const testNum =parseInt(document.querySelector('#askTimeCheck').value)
         //color per number
-        console.log('sampleSize', sampleSize, 'test number', testNum)
       const colors = ['green', 'red', 'black', 'red', 'black','red', 'black', 'red', 'black', 'red', 'black', 'black', 'red','black', 'red', 'black', 'red', 'black', 'red', 'red', 'black' ,'red', 'black', 'red', 'black', 'red', 'black', 'red', 'black','black', 'red', 'black', 'red', 'black', 'red', 'black', 'red', 'green']
         //prepare for the loop
         let difStatHolder = 0
@@ -15,50 +14,33 @@ function roll(){
         let sampleNum = 0
         let hit = false
         while(sampleNum != sampleSize) {
-            sampleNum ++
-            console.log('attempt number', sampleNum)
-            console.log('how many atetempts did the user ask for', sampleSize) 
-            console.log('will the program stop', sampleNum == sampleSize)     
+            sampleNum ++    
             const rando = Math.random()*colors.length - 1 
             const roundo = Math.round(rando)
             let results = colors[roundo]
-            console.log('number', rando)
-            console.log('rounded number', roundo)
-            console.log('results of rounded number', results)
             if (hit === true) {
                 if(results == counterColor){
                     sameStatHolder ++
-                    console.log('current amount of of times the color was the same as chunck of colors', sameStatHolder)
                 }else{
                     difStatHolder ++
                     hit = false
-                    console.log('current amount of times the color was different from the chunck of colors', difStatHolder)
                 }
             }
             if(results == counterColor){ 
                 if(counterNumber !== testNum){
                     counterNumber ++
-                    console.log('counter color is ', counterColor)
-                    console.log('counter number is', counterNumber)
                 }
                 if(counterNumber == testNum){     
                     hit = true   
-                    console.log('counter color is ', counterColor)
-                    console.log('counter number is', counterNumber)    
-                    console.log('time to record')       
                 }
             }else{
                 counterNumber = 1
                 counterColor = results
-                console.log('results did not = counter number')
-                console.log('counter number is 1')
-                console.log('counter color is', results)
             }
         }
-        console.log('amount of times that it was same', sameStatHolder)
-        console.log('the amount of times it was different', difStatHolder)
-        console.log('the amount of times you would have to watch for teh oppertunity to arise', sampleNum)
+        const events = sameStatHolder + difStatHolder
+        const wait = sampleNum / events
         document.querySelector('#sameColor').innerHTML = `${sameStatHolder}`
         document.querySelector('#diffColor').innerHTML = `${difStatHolder}`
-        document.querySelector('#timesRolled').innerHTML = `${sampleNum}`
+        document.querySelector('#timesRolled').innerHTML = `${wait}`
     }
